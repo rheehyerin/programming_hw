706d1ebf7ed833ae29573b8483d956747ca1d52c
Chinseok Lee <allieuslee@gmail.com>
2016-07-14 14:32:49 +0900

    webtoon 앱 추가 및 네이버웹툰 "고수" 크롤링

---
+++ programming/test_webtoon.py
@@ -0,0 +1,35 @@
+import os
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'programming.settings')
+import django
+django.setup()
+
+from webtoon.models import Episode
+
+from urllib.parse import urljoin
+import requests
+from bs4 import BeautifulSoup
+
+def main():
+    for page in range(1, 10000):
+        params = {
+            'titleId': 662774,
+            'page': page,
+        }
+        page_url = 'http://comic.naver.com/webtoon/list.nhn'
+        html = requests.get(page_url, params=params).text
+        soup = BeautifulSoup(html, 'html.parser')
+        for a_tag in soup.select('.viewList .title a'):
+            title = a_tag.text
+            link = urljoin(page_url, a_tag['href'])
+            print(title, link)
+            try:
+                Episode.objects.get(url=link)
+                print('End')
+                return
+            except Episode.DoesNotExist:
+                Episode.objects.create(title=title, url=link)
+
+
+if __name__ == '__main__':
+    main()
+
