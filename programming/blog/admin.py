6fe0ff45f561f0255dd9bebbf3514f7969983386
Chinseok Lee <allieuslee@gmail.com>
2016-07-19 15:28:16 +0900

    blog 앱, Tag 모델 정의 및 amdin 등록

--- programming/blog/admin.py
+++ programming/blog/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from blog.models import Post
+from blog.models import Post, Tag
 
 
 class PostAdmin(admin.ModelAdmin):
@@ -7,3 +7,5 @@ class PostAdmin(admin.ModelAdmin):
 
 admin.site.register(Post, PostAdmin)
 
+
+admin.site.register(Tag)
--- programming/blog/models.py
+++ programming/blog/models.py
@@ -13,8 +13,16 @@ class Post(models.Model):
     author = models.CharField(max_length=20)
     title = models.CharField(max_length=100, verbose_name='제목')
     content = models.TextField(help_text='Markdown 문법을 써주세요.')
-    tags = models.CharField(max_length=100, blank=True)
+    # tags = models.CharField(max_length=100, blank=True)
+    tag_set = models.ManyToManyField('Tag', blank=True)
     lnglat = models.CharField(max_length=50, validators=[lnglat_validator], help_text='경도,위도 포맷으로 입력')
     created_at = models.DateTimeField(default=timezone.now)
     test_field = models.IntegerField(default=10)
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.name
 
